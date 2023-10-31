b310e6e23755418c2a332e264cc08d4e2970d385
Alberto <alberto.bregliano@pm.me>
2023-10-31 10:54:17 +0100

    randomurl

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 un web server che si spenge in automatico dopo tot tempo o dopo che un handler è stato chiamato
 
 ## Use case
-Vuoi comunicare una cosa  a qualcuno?
-Cosa c'è di meglio di un pagina server che si può vedere una volta sola?
-Magari sicurizzata mtls?
+Vuoi comunicare una cosa a qualcuno?
+
+Cosa c'è di meglio di un pagina http che si può vedere una volta sola?
+
 Se poi nessuno vi accede per tot tempo il server si spenge in automatico.
+
 Potrebbe essere utile per diminuire la superficie di attacco dei servizi IT
 
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 	"flag"
 	"log"
 	"net/http"
@@ -18,14 +21,25 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
+	str := randomString()
+
 	router := http.DefaultServeMux
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/"+str, func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 		cancel()
 	})
 
 	go http.ListenAndServe(":"+port, router)
 	log.Printf("Avviato web server su porta: %s, resterà attivo per %v\n", port, timeout)
+	log.Println("http://127.0.0.1" + ":" + port + "/" + str)
 	<-ctx.Done()
 	log.Println(ctx.Err())
+}
+
+func randomString() string {
+	b := make([]byte, 200)
+	rand.Read(b)
+	h := sha256.New()
+	h.Write(b)
+	return hex.EncodeToString(h.Sum(nil))
 }
